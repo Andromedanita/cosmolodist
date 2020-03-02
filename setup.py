1617ed2af80329f41d1e0286db9d22b1349d8629
Anita Bahmanyar <anita@Anitas-MacBook-Pro.local>
2020-03-02 14:35:39 -0500

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="cosmolodist",
+    version="0.0.1",
+    author="Anita Bahmanyar",
+    author_email="bahmanyar@astro.utoronto.ca",
+    description="A package to calculate cosmology distances for different cosmologies",
+    packages=["cosmolodist"]
+)
